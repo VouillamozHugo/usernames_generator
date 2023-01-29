f4ac4ed649e351c8bb6ff69ca1e7357c98806b1f
Vouillamoz_Hugo <73638403+VouillamozHugo@users.noreply.github.com>
2023-01-29 18:47:47 +0100

    add time and full upper case
    Count the number of line created and add full upper case username

--- username_generator.py
+++ username_generator.py
@@ -1,11 +1,13 @@
-import sys
+import sys, time
+
 
 def getUsername():
-
-    print("Usernames file found \n"
-          "Start generating username variation\n###############")
+    print("Usernames file found \n\n" + 40 * "#" + "\n#  Start generating username variation  #\n" + 40 * "#" + "\n")
     with open(usernameFile, 'r') as i:
         lines = i.readlines()
+        with open(fileToWrite, 'w') as f:
+            f.writelines("")
+
         for name in lines:
             try:
                 if name.strip():
@@ -15,20 +17,21 @@ def getUsername():
                     letterFirstname = fname[0]
                     letterLastname = lname[0]
 
-                    generateVariation(fname,lname,letterFirstname,letterLastname)
-                    generateVariation(lname,fname,letterLastname,letterFirstname)
+                    generateVariation(fname, lname, letterFirstname, letterLastname)
+                    generateVariation(lname, fname, letterLastname, letterFirstname)
 
                     letterFirstname = letterFirstname.upper()
                     letterLastname = letterLastname.upper()
-                    generateVariation(fname,lname,letterFirstname,letterLastname)
-                    generateVariation(lname,fname,letterLastname,letterFirstname)
+                    generateVariation(fname, lname, letterFirstname, letterLastname)
+                    generateVariation(lname, fname, letterLastname, letterFirstname)
+                    fname.upper()
+                    lname.upper()
+                    generateVariation(fname, lname, letterFirstname, letterLastname)
+                    generateVariation(lname, fname, letterLastname, letterFirstname)
 
             except Exception as e:
                 print("An error occured while reading the usernames file\n"
                       "Be sure that all username are in the following structure => firstname lastname ", flush=True)
-
-
-
 
 
 def generateVariation(fname, lname, letterFirstname, letterLastname):
@@ -47,27 +50,31 @@ def generateVariation(fname, lname, letterFirstname, letterLastname):
     mutations.append(letterFirstname + "." + letterLastname + "\n")
     mutations.append(letterFirstname + " " + letterLastname + "\n")
     mutations.append(letterFirstname + letterLastname + "\n")
-    mutations.append(letterFirstname +  fname + letterLastname + "\n")
-    mutations.append(letterFirstname +  fname + "." + letterLastname + "\n")
+    mutations.append(letterFirstname + fname + letterLastname + "\n")
+    mutations.append(letterFirstname + fname + "." + letterLastname + "\n")
     mutations.append(letterFirstname + fname + " " + letterLastname + lname + "\n")
-    mutations.append(letterFirstname + fname  + letterLastname + lname + "\n")
-
-
+    mutations.append(letterFirstname + fname + letterLastname + lname + "\n")
     with open(fileToWrite, 'a') as f:
         f.writelines(mutations)
 
 
 if __name__ == '__main__':
+    start = time.time()
     try:
         usernameFile = sys.argv[1]
         fileToWrite = sys.argv[2]
     except:
-        print("\nTo use the Username_builder.py you need to give the following arguments :\n" \
-        "- Path to the list of username \n" \
-        "- Name of the file for output \n" \
-        "\n" \
-        "Example of use : python username_builder.py username.txt outputWordlist.txt \n")
+        print("\nTo use the Username_builder.py you need to give the following arguments :\n" 
+              "- Path to the list of username \n" 
+              "- Name of the file for output \n" 
+              "\n" 
+              "Example of use : python username_builder.py username.txt outputWordlist.txt \n")
         sys.exit()
     getUsername()
-    print("\rThe scripts finished successfully !\n"
-          "Happy hacking ;) ", flush=True)
+    end = time.time()
+    with open(fileToWrite, 'r') as f:
+        count = len(f.readlines())
+    duration = round(end - start, 4)
+    print("\rThe scripts finished successfully !")
+    print(str(count) + " lines where generated in " + str(duration) + " seconde")
+    print("Happy hacking ;) ", flush=True)
